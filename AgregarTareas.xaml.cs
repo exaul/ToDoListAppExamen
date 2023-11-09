8e3ffda8c80c4945fae1e78075d3d0c302ab664b
exaul <edwin.falcones@tecnologicoliceocristiano.edu.ec>
2023-11-09 16:09:28 -0500

    ahora si esta con binding

--- AgregarTareas.xaml.cs
+++ AgregarTareas.xaml.cs
@@ -2,24 +2,33 @@ namespace ToDoListApp;
 
 public partial class AgregarTareas : ContentPage
 {
-	public AgregarTareas()
+    public Tasks NuevaTarea { get; set; }
+    public AgregarTareas()
 	{
 		InitializeComponent();
-	}
+        NuevaTarea = new Tasks();
+        BindingContext = this;
+    }
 
     private void ConfirmarBtn_Clicked(object sender, EventArgs e)
     {
-        string taskName = TaskNameEntry.Text;
-        string status = StatusEntry.Text;
-        // se envian los datos 
-        
-        MessagingCenter.Send(this, "AgregarTarea", new Tasks
-        {
-            TaskName = taskName,
-            Status = status
-        });
-
-        // Cerrar la página actual
+        //metodo con binding
+        MessagingCenter.Send(this, "AgregarTarea", NuevaTarea);
         Navigation.PopAsync();
+
+        
     }
+    //metodo sin binding 
+    /*string taskName = TaskNameEntry.Text;
+    string status = StatusEntry.Text;
+    // se envian los datos 
+
+    MessagingCenter.Send(this, "AgregarTarea", new Tasks
+    {
+        TaskName = taskName,
+        Status = status
+    });
+
+    // Cerrar la página actual
+    Navigation.PopAsync();*/
 }
